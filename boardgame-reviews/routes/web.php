584ebc52b9704730645f7f107a0297241829e2fb
Kobatai <taiga1983346@gmail.com>
2020-01-07 23:07:03 +0900

    初期設定とphpのバージョンup

--- boardgame-reviews/routes/web.php
+++ boardgame-reviews/routes/web.php
@@ -1,16 +1,11 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// SPAのため全部のルーティングはindexに飛ばす
+// any パラメータはあってもなくてもいい（?）し、ある場合はどんな文字列でもいい（.+）ということ
+// アロー関数はphp7.4から
+Route::get('/{any?}', fn() => view('index'))->where('any', '.+');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// それ以前の書き方
+// Route::get('/{any?}', function () {
+//     return view('index');
+//  })->where('any', '.+');
