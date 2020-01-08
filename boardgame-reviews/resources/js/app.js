3ebd261f6b2f6b2fa14de5e8f9b26cc312bdc102
Kobatai <taiga1983346@gmail.com>
2020-01-08 17:32:27 +0900

    Appのrouting

--- boardgame-reviews/resources/js/app.js
+++ boardgame-reviews/resources/js/app.js
@@ -1,6 +1,11 @@
-import Vue from "vue";
+import Vue from 'vue'
+import router from './router'
+// ルートコンポーネント
+import App from './App.vue'
 
 new Vue({
-    el: "#app",
-    template: "<h1>Hello world</h1>"
-});
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App />'
+})
