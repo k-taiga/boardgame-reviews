11983849c57f2ba16632e2a9dc7aab1cf3d312e8
Kobatai <taiga1983346@gmail.com>
2020-01-13 12:18:44 +0900

    auth register

--- boardgame-reviews/resources/js/bootstrap.js
+++ boardgame-reviews/resources/js/bootstrap.js
@@ -2,11 +2,11 @@ import { getCookieValue } from "./util";
 
 window.axios = require("axios");
 
-// Ajaxリクエストのヘッダを付与
+// Ajaxリクエストであることを示すヘッダーを付与する
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 window.axios.interceptors.request.use(config => {
-    // クッキーからトークンを取り出してヘッダーに付与する
+    // クッキーからトークンを取り出してヘッダーに添付する
     config.headers["X-XSRF-TOKEN"] = getCookieValue("XSRF-TOKEN");
 
     return config;
