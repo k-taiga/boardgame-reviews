3610d01ffdef2bd224ecc7c4c2867df4f27919df
Kobatai <taiga1983346@gmail.com>
2020-01-13 12:18:44 +0900

    auth register

--- boardgame-reviews/resources/js/views/SignIn.vue
+++ boardgame-reviews/resources/js/views/SignIn.vue
@@ -1,89 +1,76 @@
 <template>
-  <div class="hero is-light is-fullheight">
-    <div class="hero-body">
-      <div class="container has-text-centered">
-        <article v-show="errorMessage" class="message is-warning">
-          <div class="message-body">{{ error_message }}</div>
-        </article>
-        <div class="column is-4 is-offset-4">
-          <div class="box">
-            <bdTextField type="email" placeholder="メールアドレス" icon="envelope" v-model="email"></bdTextField>
-            <!-- パスワードの入力欄としてテキストフィールドコンポーネントを使用 -->
-            <bdTextField type="password" placeholder="パスワード" icon="lock" v-model="password"></bdTextField>
-            <div class="field">
-              <label class="checkbox">
-                <input type="checkbox" />
-                ログインしたままにする
-              </label>
+    <div class="hero is-light is-fullheight">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <article v-show="errorMessage" class="message is-warning">
+                    <div class="message-body">{{ error_message }}</div>
+                </article>
+                <form class="form" @submit.prevent="signUp">
+                    <div class="column is-4 is-offset-4">
+                        <div class="box">
+                            <bdTextField
+                                type="email"
+                                placeholder="メールアドレス"
+                                icon="envelope"
+                                v-model="email"
+                            ></bdTextField>
+                            <!-- パスワードの入力欄としてテキストフィールドコンポーネントを使用 -->
+                            <bdTextField
+                                type="password"
+                                placeholder="パスワード"
+                                icon="lock"
+                                v-model="password"
+                            ></bdTextField>
+                            <div class="field">
+                                <label class="checkbox">
+                                    <input type="checkbox" />
+                                    ログインしたままにする
+                                </label>
+                            </div>
+                            <button
+                                class="button is-block is-info is-large is-fullwidth"
+                                @click.prevent="signIn"
+                            >
+                                ログインする
+                            </button>
+                        </div>
+                    </div>
+                </form>
+                <p class="has-text-grey">
+                    <a href="..">パスワードを忘れた方はこちら</a>
+                </p>
             </div>
-            <button
-              class="button is-block is-info is-large is-fullwidth"
-              @click.prevent="signIn"
-            >ログインする</button>
-          </div>
         </div>
-        <p class="has-text-grey">
-          <a href="..">パスワードを忘れた方はこちら</a>
-        </p>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
 import bdTextField from "../components/TextField";
 
 export default {
-  name: "sign_in",
-  components: { bdTextField },
-  data() {
-    return {
-      email: "",
-      password: "",
-      error_message: null
-    };
-  },
-  methods: {
-    login() {
-      this.$store.dispatch("auth/create", {
-        user: {
-          email: this.email,
-          password: this.password
-        }
-      });
+    name: "sign_in",
+    components: { bdTextField },
+    data() {
+        return {
+            email: "",
+            password: "",
+            error_message: null
+        };
     },
-    signIn(e) {
-      alert(`Email: ${this.email}, Password: ${this.password}`);
-      this.email = null;
-      this.password = null;
+    methods: {
+        login() {
+            this.$store.dispatch("auth/create", {
+                user: {
+                    email: this.email,
+                    password: this.password
+                }
+            });
+        },
+        signIn(e) {
+            alert(`Email: ${this.email}, Password: ${this.password}`);
+            this.email = null;
+            this.password = null;
+        }
     }
-  }
 };
-//   },
-//   computed: {
-//     isValidated () {
-//       return Object.keys(this.fields).every(k => this.fields[k].validated) &&
-//         Object.keys(this.fields).every(k => this.fields[k].valid)
-//     },
-//     token () {
-//       return this.$store.state.auth.token
-//     },
-//     errorMessage () {
-//       return this.$store.state.message.error
-//     }
-//   },
-//   created: function () {
-//     this.$store.dispatch('message/destroy')
-//     // already logined
-//     if (this.$store.state.auth.token) {
-//       this.$router.push('/')
-//     }
-//   },
-//   watch: {
-//     token (newToken) {
-//       this.$router.push('/')
-//     }
-//   }
-// }
 </script>
-
