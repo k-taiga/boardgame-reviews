1c567f360733dd88647aa5bb82c4528f9c36c718
Kobatai <taiga1983346@gmail.com>
2020-01-19 17:01:02 +0900

    photo submit

--- boardgame-reviews/app/Http/Controllers/PhotoController.php
+++ boardgame-reviews/app/Http/Controllers/PhotoController.php
@@ -18,4 +18,43 @@ class PhotoController extends Controller
         // 認証に通す
         $this->middleware('auth');
     }
+
+    /**
+     * 写真投稿
+     * @param StorePhoto $request
+     * @return \Illuminate\Http\Response
+     */
+    public function create(StorePhoto $request)
+    {
+        // 写真の拡張子を取得
+        $extension = $request->photo->extension();
+
+        $photo = new Photo();
+
+        // インスタンス生成時に割り振られたランダムなID値と本来の拡張子を組み合わせてファイル名とする
+        $photo->filename = $photo->id . '.' . $extension;
+
+        // S3にファイルを保存する publicで公開
+        Storage::cloud()
+            ->putFileAs('', $request->photo, $photo->filename, 'public');
+
+        // データベースエラー時にファイル削除を行うため
+        // トランザクションを利用する
+        DB::beginTransaction();
+
+        try {
+            Auth::user()->photos()->save($photo);
+            DB::commit();
+        } catch (\Exception $exception) {
+            DB::rollBack();
+            // DBとの不整合を避けるためアップロードしたファイルを削除
+            Storage::cloud()->delete($photo->filename);
+            throw $exception;
+        }
+
+        // リソースの新規作成なので
+        // レスポンスコードは201(CREATED)を返却する
+        // vueでキャッチする
+        return response($photo, 201);
+    }
 }
--- boardgame-reviews/resources/js/views/Home.vue
+++ boardgame-reviews/resources/js/views/Home.vue
@@ -1,3 +1,33 @@
 <template>
-  <div class="container"></div>
+    <div class="container">
+        <div class="hero is-fullheight">
+            <button
+                class="button is-info is-rounded is-hovered"
+                @click="showForm"
+            >
+                <i class="icon ion-md-add"></i>
+                Submit a photo
+            </button>
+            <div class="hero-body">
+                <photoForm v-model="show" />
+            </div>
+        </div>
+    </div>
 </template>
+
+<script>
+import photoForm from "../components/PhotoForm.vue";
+export default {
+    data() {
+        return {
+            show: false
+        };
+    },
+    components: { photoForm },
+    methods: {
+        showForm() {
+            this.show = !this.show;
+        }
+    }
+};
+</script>
