33b6633f5c6a207b0cb309b332549172424a782e
Kobatai <taiga1983346@gmail.com>
2020-01-31 10:01:47 +0900

    duskインストール

---
+++ boardgame-reviews/tests/Browser/ExampleTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+use Laravel\Dusk\Chrome;
+
+class ExampleTest extends DuskTestCase
+{
+    /**
+     * A basic browser test example.
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                ->assertSee('Laravel');
+        });
+    }
+}
