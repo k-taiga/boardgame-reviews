9a5a515d8c71381b42ccc3a0373c8bac47f52bc9
Kobatai <taiga1983346@gmail.com>
2020-03-07 16:49:40 +0900

    routes整理

--- boardgame-reviews/routes/api.php
+++ boardgame-reviews/routes/api.php
@@ -22,23 +22,18 @@ Route::post('/photos', 'PhotoController@create')->name('photo.create');
 Route::post('/shops', 'ShopController@create')->name('shop.create');
 
 // 店舗一覧
-// Route::get('/photos', 'PhotoController@index')->name('photo.index');
 Route::get('/shops', 'ShopController@index')->name('shop.index');
 
 // 店舗詳細
-// Route::get('/photos/{id}', 'PhotoController@show')->name('photo.show');
 Route::get('/shops/{id}', 'ShopController@show')->name('shop.show');
 
 // レビュー
-// Route::post('/photos/{photo}/reviews', 'PhotoController@addReview')->name('photo.review');
 Route::post('/shops/{shop}/reviews', 'ShopController@addReview')->name('shop.review');
 
 // いいね
-// Route::put('/photos/{id}/like', 'PhotoController@like')->name('photo.like');
 Route::put('/shops/{id}/like', 'ShopController@like')->name('shop.like');
 
 // いいね解除
-// Route::delete('/photos/{id}/unlike', 'PhotoController@unlike');
 Route::delete('/shops/{id}/unlike', 'ShopController@unlike');
 
 // トークンリフレッシュ
