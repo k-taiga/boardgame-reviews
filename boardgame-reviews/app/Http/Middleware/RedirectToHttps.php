2a4ff63a8f447187db53a9b5e324d212cdce7068
Kobatai <taiga1983346@gmail.com>
2020-04-07 14:22:46 +0900

    .htaccess追加

--- boardgame-reviews/app/Http/Middleware/RedirectToHttps.php
+++ boardgame-reviews/app/Http/Middleware/RedirectToHttps.php
@@ -15,6 +15,7 @@ class RedirectToHttps
      */
     public function handle($request, Closure $next)
     {
-        return redirect('https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+        // return redirect('https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+        return redirect()->secure($request->getRequestUri());
     }
 }
