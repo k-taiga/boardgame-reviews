9d359a196d4617be28ef358b6ae050c5601370d3
Kobatai <taiga1983346@gmail.com>
2020-06-05 20:27:52 +0900

    update: oAuthCallback'のテスト修正

--- boardgame-reviews/tests/Feature/OAuthTest.php
+++ boardgame-reviews/tests/Feature/OAuthTest.php
@@ -6,13 +6,49 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
+use App\User;
+use Auth;
+use Socialite;
+use Mockery;
+
 class OAuthTest extends TestCase
 {
     public function setUp():void
     {
       parent::setUp();
+
+      // Mockに対してそのクラスに本来存在しないメソッドが追加された場合にエラーにするかどうかの設定
+      Mockery::getConfiguration()->allowMockingNonExistentMethods(false);
+
       $this->providerName = "google";
+
+      // モックを作成
+      // SocialiteのUserクラスのモックを作成
+      $this->user = Mockery::mock('Laravel\Socialite\Two\User');
+
+      $this->user
+      //getId()メソッドにuniqidを返すように設定
+      ->shouldReceive('getId')
+      ->andReturn(uniqid())
+      ->shouldReceive('getEmail')
+      ->andReturn(uniqid().'@test.com')
+      ->shouldReceive('getNickname')
+      ->andReturn('Pseudo');
+
+      // Socialiteのproviderのモックを作成
+      $this->provider = Mockery::mock('Laravel\Socialite\Contracts\Provider');
+      // providerのuserメソッドでこのモックのuserを返す
+      $this->provider->shouldReceive('user')->andReturn($this->user);
+
     }
+
+    // :voidは返り値がなし
+    public static function tearDownAfterClass(): void
+    {
+        // Mockeryの設定をもとに戻す
+        Mockery::getConfiguration()->allowMockingNonExistentMethods(true);
+    }
+
 
     /**
      * test
@@ -43,8 +79,24 @@ class OAuthTest extends TestCase
      */
     public function test_Googleアカウントでユーザー登録できる()
     {
-      $this->get(route('oAuthCallback', ['provider' => $this->providerName]))
-        ->assertStatus(200);
+      // withでshouldReceiveの引数を指定　そして返す中身はandReturnで指定
+      Socialite::shouldReceive('driver')->with($this->providerName)->andReturn($this->provider);
+
+      // URLをコール
+      $this->get(route('oauthCallback', ['service' => $this->providerName]))
+          ->assertStatus(302)
+          ->assertRedirect(route('home'));
+
+      // 各データが正しく登録されているかチェック
+      $this->assertDatabaseHas('users', [
+          'provider_id' => $this->user->getId(),
+          'provider_name' => $this->providerName,
+          'name' => $this->user->getNickName(),
+          'email' => $this->user->getEmail()
+      ]);
+
+      // 認証チェック
+      $this->assertAuthenticated();
     }
 
 }
